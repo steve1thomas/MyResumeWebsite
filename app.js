7dd460a19f5a7add22cf67528db9ccefe18f0eef
steve thomas <set5323@gmail.com>
2017-07-18 19:51:58 -0400

    Adding project files from local repo

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.getElementByID('registrar');
+const input = form.querySelector('input');
+form.addEventListener('submit',(e) = > {
+  console.log(input.value);});
+const text=input.value;
+input.value="";
+});
