a376ad271598bfec5b2681688bdff9ea8ea9d981
steve1thomas <set5323@gmail.com>
2017-07-21 08:16:42 -0400

    update comment line

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-/* ================================= 
+<!-- ================================= 
   JScript Element refresh 
-==================================== */
+==================================== -->
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 form.addEventListener('submit',(e) => {
