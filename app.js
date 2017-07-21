20f99f2ec618dfcc86c668d4eeed48e7034326a0
steve1thomas <set5323@gmail.com>
2017-07-21 08:45:16 -0400

    added phone number to alert

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const input = form.querySelector('input');
 form.addEventListener('submit',(e) => {
    e.preventDefault ();
 const text=input.value;
- alert('Thank you for your Name and Number, will be in Contact with you!');
+ alert('Thank you for your Name and PhoneNumber, will be in Contact with you!');
   input.value="";
 });
