c5bc02986f6c16631932264d2b4879ef007503e6
steve1thomas <set5323@gmail.com>
2017-07-21 07:51:15 -0400

    update spelling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* ================================= 
   JScript Element refresh 
 ==================================== */
-const form = document.getElementByID('registrar');
+const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 form.addEventListener('submit',(e) = > {
    e.preventDefault ();
